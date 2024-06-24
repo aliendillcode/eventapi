1fde1e50c444fbc2552479e4c6a47e71f2ce3708
sgtdill <sgtdill@gmail.com>
2024-06-24 16:43:30 -0400

    feat: creating flyway files

--- src/main/resources/db/migration/V2__create-coupon-tabel.sql
+++ src/main/resources/db/migration/V2__create-coupon-tabel.sql
@@ -1,7 +1,8 @@
 CREATE TABLE coupon(
     id UUID DEFAULT gen_random_uuid()PRIMARY KEY,
     code VARCHAR(50) NOT NULL,
-    descount INTEGER NOT NULL,
+    discount INTEGER NOT NULL,
     valid TIMESTAMP NOT NULL,
-
+    event_id UUID,
+    FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE
 )
